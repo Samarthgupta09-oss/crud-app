bcdfc439865d1ed34425654db9ff224e33ac8828
Samarthgupta09-oss <samarth.gupta_cs.aiml24@gla.ac.in>
2026-02-10 15:07:54 +0530

    validation added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///employee.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+app.config["SECRET_KEY"] = "supersecretkey"
 
 db = SQLAlchemy(app)
 app.app_context().push()
@@ -16,11 +17,20 @@ class Employee(db.Model):
 @app.route("/", methods=['GET', 'POST'])
 def home():
    if request.method == 'POST':
-      name = request.form['name']
-      email = request.form['email']
+      
+      name= request.form.get('name','').strip()
+      email = request.form.get('email','').strip()
+    
+      if not name or not email :
+         flash("All fields are required!", "danger")
+         return redirect("/")
       employee = Employee(name = name, email = email)
       db.session.add(employee)
       db.session.commit()
+      
+      flash("Employee added successfully!", "success")
+      return redirect("/")
+
    allemployee = Employee.query.all()	
    return render_template("index.html", allemployee=allemployee)
 
@@ -28,13 +38,18 @@ def home():
 def about():
    return render_template("about.html")
 
+@app.route("/contact")
+def contact():
+   return render_template("contact.html")
+
+
 @app.route("/delete/<int:sno>")
 def delete(sno):
     employee = Employee.query.filter_by(sno=sno).first()
     db.session.delete(employee)
     db.session.commit()
     return redirect("/")
-
+    
 @app.route("/update/<int:sno>", methods=['GET', 'POST'])
 def update(sno):
     if request.method=='POST':
@@ -42,14 +57,13 @@ def update(sno):
         email = request.form['email']
         employee = Employee.query.filter_by(sno=sno).first()
         employee.name = name
-        employee.email = email
+        employee.email=email
         db.session.add(employee)
         db.session.commit()
         return redirect("/")
 
     employee = Employee.query.filter_by(sno=sno).first()
     return render_template("update.html", employee=employee)
-
 
 if __name__ == '__main__':
    app.run(debug=True)
